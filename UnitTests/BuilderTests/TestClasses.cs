a9a32659da10e30180f1f3751d62391f19b4208b
Philip Peitsch <philip.peitsch@gmail.com>
2010-05-20 22:07:36 +1000

    * Splitting Builder tests for up to make them more manageable. * Adding unit test to show issue with buliding inside parent and child containers

---
+++ UnitTests/BuilderTests/TestClasses.cs
@@ -0,0 +1,95 @@
+using System;
+
+namespace NGineer.UnitTests.BuilderTests
+{
+    public class CountsPropertySets
+    {
+        private int _somePropertySets;
+        public int GetSomePropertySets()
+        {
+            return _somePropertySets;
+        }
+
+        private int _someProperty;
+        public int SomeProperty
+        {
+            get { return _someProperty; }
+            set
+            {
+                _someProperty = value;
+                _somePropertySets++;
+            }
+        }
+
+        private int _recursivePropertySets;
+        public int GetRecursivePropertySets()
+        {
+            return _recursivePropertySets;
+        }
+
+        private CountsPropertySets _recursiveProperty;
+        public CountsPropertySets RecursiveProperty
+        {
+            get { return _recursiveProperty; }
+            set
+            {
+                _recursiveProperty = value;
+                _recursivePropertySets++;
+            }
+        }
+    }
+
+
+    public class RecursiveClass
+    {
+        public int IntProperty { get; set; }
+        public RecursiveClass RecursiveReference { get; set; }
+    }
+
+    public class SimpleClass
+    {
+        public int IntProperty { get; set; }
+        public string StringProperty { get; set; }
+        public TestClass2 TestClass2Property { get; set; }
+    }
+
+    public class TestClass2
+    {
+		public string StringProperty { get; set; }
+    }
+
+    public class ClassWithNullableInt
+    {
+        public int? Property1 { get; set; }
+    }
+
+    public class ClassWithNullableDateTime
+    {
+        public DateTime? Property1 { get; set; }
+    }
+
+    public class InheritsFromClassWithNullableDateTime : ClassWithNullableDateTime
+    {
+    }
+
+    public class InheritsFromClassWithNullableDateTime2 : ClassWithNullableDateTime
+    {
+    }
+
+    public class TestClass
+    {
+        public int Property1 { get; set; }
+
+        public TestClass2 Property2 { get; set; }
+    }
+	
+	public class TestClassThreeDeep
+	{
+		public TestClass PropertyTestClass { get; set; }
+	}
+	
+	public class TestClassFourDeep
+	{
+		public TestClassThreeDeep PropertyTestClass { get; set; }
+    }
+}
