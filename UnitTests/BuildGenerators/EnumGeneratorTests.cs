8b038441f76120df76c273ea148a46de57260f1f
Philip Peitsch <philip.peitsch@gmail.com>
2010-06-22 22:33:06 +1000

    EnumGeneratorTests now primarily ensures multiple calls within the same session do not return all the same enum values

--- UnitTests/BuildGenerators/EnumGeneratorTests.cs
+++ UnitTests/BuildGenerators/EnumGeneratorTests.cs
@@ -30,10 +30,31 @@ namespace NGineer.UnitTests.BuildGenerators
             Generator = new EnumGenerator();
             var enumValue = Generator.Create(typeof(TestEnum), null, BuildSession());
             Assert.IsNotNull(enumValue);
-            // I just happen to know that a seed of 2 will produce this result...
-            Assert.AreEqual(TestEnum.Value3, enumValue);
+			// can't think of a reliable way to tell wat the produced results will be...
         }
 
+		[Test]
+        public void GenerateSimpleEnum_ValueChanges()
+        {
+			var valueChanged = false;
+			var lastValue = TestEnum.Value1;
+			const int maxTries = 10;
+            Generator = new EnumGenerator();
+			var session = BuildSession();
+			for(int tries = 0; tries < maxTries; tries++)
+			{
+            	var enumValue = (TestEnum)Generator.Create(typeof(TestEnum), null, session);
+            	Assert.IsNotNull(enumValue);
+				if(tries > 0 && lastValue != enumValue)
+				{
+					valueChanged = true;
+					break;
+				}
+				lastValue = enumValue;
+			}
+			Assert.IsTrue(valueChanged, "EnumGenerator always returns identical value");
+        }
+		
         public enum TestEnum
         {
             Value1,
