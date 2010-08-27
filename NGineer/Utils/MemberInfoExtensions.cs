83bff8d148c9311fcc27c03c4efa850023286a00
Philip Peitsch <philip.peitsch@gmail.com>
2010-08-27 23:52:14 +1000

    Removing unneeded code

--- NGineer/Utils/MemberInfoExtensions.cs
+++ NGineer/Utils/MemberInfoExtensions.cs
@@ -5,29 +5,6 @@ namespace NGineer.Utils
 {
 	public static class MemberInfoExtensions
 	{
-		public static bool LikelyEqual(this MemberInfo self, MemberInfo other)
-		{
-			return other != null
-				&& self.Name == other.Name
-				&& self.MemberType == other.MemberType
-				&& self.DeclaringType == other.DeclaringType
-				&& self.MemberType == MemberTypes.Property ? 
-					LikelyEqual(self as PropertyInfo, other as PropertyInfo)
-					: LikelyEqual(self as FieldInfo, other as FieldInfo);
-		}
-		
-		private static bool LikelyEqual(this PropertyInfo self, PropertyInfo other)
-		{
-			return other != null
-				&& self.PropertyType == other.PropertyType;
-		}
-		
-		private static bool LikelyEqual(this FieldInfo self, FieldInfo other)
-		{
-			return other != null
-				&& self.FieldType == other.FieldType;
-		}
-
         public static Type ReturnType(this MemberInfo self)
         {
             if (self.MemberType == MemberTypes.Field)
