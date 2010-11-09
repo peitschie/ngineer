a5439b1d0dff014a89f9cc4f3d8a178693ef33f6
Philip Peitsch <philip.peitsch@gmail.com>
2010-11-10 10:57:09 +1100

    Added test demonstrating incorrect generator behaviour

--- UnitTests/BuilderTests/WithGeneratorTests.cs
+++ UnitTests/BuilderTests/WithGeneratorTests.cs
@@ -10,6 +10,34 @@ namespace NGineer.UnitTests.BuilderTests
 	[TestFixture]
 	public class WithGeneratorTests
 	{
-		
+	    private IBuilder _builder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _builder = new Builder();
+        }
+
+        [Test]
+		public void CustomGeneratedObject_IsNotThenPopulated()
+		{
+            _builder.WithGenerator(() => new ThrowsOnAccess());
+            ThrowsOnAccess result = null;
+            result = _builder.Build<ThrowsOnAccess>();
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Property2);
+		}
+
+        private class ThrowsOnAccess
+        {
+            private int _property;
+            public int Property
+            {
+                get { throw new Exception("Property should not have been accessed"); }
+                set { throw new Exception("Property should not have been accessed"); }
+            }
+
+            public string Property2 { get; set; }
+        }
 	}
 }
