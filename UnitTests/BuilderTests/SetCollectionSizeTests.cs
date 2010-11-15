1aa2c7114778db83917bbed8d3f949821a6a5607
Philip Peitsch <philip.peitsch@gmail.com>
2010-11-15 19:58:49 +1100

    More tests added around SetCollectionSize.  These current fail due to the same bug that afflicts SetNumberOfInstances

---
+++ UnitTests/BuilderTests/SetCollectionSizeTests.cs
@@ -0,0 +1,86 @@
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NGineer.UnitTests.BuilderTests
+{
+	[TestFixture]
+	public class SetCollectionSizeTests
+	{
+		[Test]
+        public void SetCollectionSize_ObjectCollectionProperlySet_Array()
+        {
+            var builder = new Builder(1)
+                .SetCollectionSize<SimpleClass>(3, 3)
+                .Sealed();
+            var simpleClassArray = builder.Build<SimpleClass[]>();
+            Assert.AreEqual(3, simpleClassArray.Length);
+        }
+
+        [Test]
+        public void SetCollectionSize_ObjectCollectionProperlySet_List()
+        {
+            var builder = new Builder(1)
+                .SetCollectionSize<SimpleClass>(3, 3)
+                .Sealed();
+            var simpleClassArray = builder.Build<List<SimpleClass>>();
+            Assert.AreEqual(3, simpleClassArray.Count);
+        }
+
+        [Test]
+        public void SetCollectionSize_ObjectCollectionProperlySet_LastSettingOverridesPrevious()
+        {
+            var builder = new Builder(1)
+                .SetCollectionSize<SimpleClass>(10, 10)
+                .SetCollectionSize<SimpleClass>(3, 3)
+                .Sealed();
+            var simpleClassArray = builder.Build<SimpleClass[]>();
+            Assert.AreEqual(3, simpleClassArray.Length);
+        }
+
+
+        [Test]
+        public void SetCollectionSize_ObjectCollectionProperlySet_InChildBuilder()
+        {
+            var builder = new Builder(1)
+                .SetCollectionSize<SimpleClass>(3, 3)
+                .For<SimpleClassParent>()
+                    .Set(x => x.Child, (obj, buildr, session) => buildr.CreateNew().Build<SimpleClass[]>())
+                    .Ignore(x => x.UniqueChild)
+                .Sealed();
+            var parentClass = builder.Build<SimpleClassParent>();
+            Assert.AreEqual(3, parentClass.Child.Length);
+        }
+
+        [Test]
+        public void SetCollectionSize_ObjectsProperlyReused_ChildBuilder_WithinChildBuilder()
+        {
+            var builder = new Builder(1)
+                .SetCollectionSize<SimpleClass>(3, 3)
+                .CreateNew()
+                .For<SimpleClassParent>()
+                    .Set(x => x.Child, (obj, buildr, session) => buildr.CreateNew().Build<SimpleClass[]>())
+                    .Ignore(x => x.UniqueChild)
+                .Sealed();
+            var parentClass = builder.Build<SimpleClassParent>();
+            Assert.AreEqual(3, parentClass.Child.Length);
+        }
+
+        [Test]
+        public void SetCollectionSize_ObjectsProperlyReused_ChildBuilder_WithinChildBuilder_OverridesParent()
+        {
+            var builder = new Builder(1)
+                .SetCollectionSize<SimpleClass>(10, 10)
+                .CreateNew()
+                .SetCollectionSize<SimpleClass>(3, 3)
+                .For<SimpleClassParent>()
+                    .Set(x => x.Child, (obj, buildr, session) => buildr.CreateNew().Build<SimpleClass[]>())
+                    .Ignore(x => x.UniqueChild)
+                .Sealed();
+            var parentClass = builder.Build<SimpleClassParent>();
+            Assert.AreEqual(3, parentClass.Child.Length);
+        }
+	}
+}
