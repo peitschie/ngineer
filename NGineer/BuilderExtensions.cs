996effe9badd84fdaa817b72d0bfcb1373d8d551
Philip Peitsch <philip.peitsch@gmail.com>
2010-11-24 21:53:54 +1100

    Cleaned up un-needed builder extensions and added API comments to each

--- NGineer/BuilderExtensions.cs
+++ NGineer/BuilderExtensions.cs
@@ -28,6 +28,16 @@ namespace NGineer
             return builder.AfterConstructionOf(new IgnoreMemberSetter(member, member.ReflectedType, allowInherited));
         }
 
+
+        /// <summary>
+        /// Throw an exception any time a plain object type is constructed
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="IBuilder"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="IBuilder"/>
+        /// </returns>
         public static IBuilder ThrowWhenGeneratingObjectType(this IBuilder builder)
         {
             return builder.WithGenerator<object>((buildr, session) => {
@@ -35,12 +45,47 @@ namespace NGineer
             });
         }
 
+        /// <summary>
+        /// Add a given key / value pair into the selected dictionary type
+        /// Other key and value pairs may already exist
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TDictType>"/>
+        /// </param>
+        /// <param name="key">
+        /// A <see cref="TKey"/>
+        /// </param>
+        /// <param name="value">
+        /// A <see cref="TElement"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TDictType>"/>
+        /// </returns>
         public static ITypedBuilder<TDictType> AddEntry<TDictType, TKey, TElement>(this ITypedBuilder<TDictType> builder, TKey key, TElement value)
             where TDictType : IDictionary<TKey,TElement>
         {
             return builder.Do(x => x[key] = value);
         }
 
+        /// <summary>
+        /// Add a given key / value pair into the selected member's dictionary
+        /// Other key and value pairs may already exist
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TType>"/>
+        /// </param>
+        /// <param name="expression">
+        /// A <see cref="Expression<Func<TType, TDictType>>"/>
+        /// </param>
+        /// <param name="key">
+        /// A <see cref="TKey"/>
+        /// </param>
+        /// <param name="value">
+        /// A <see cref="TElement"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TType>"/>
+        /// </returns>
         public static ITypedBuilder<TType> AddEntry<TType, TDictType, TKey, TElement>(this ITypedBuilder<TType> builder,
             Expression<Func<TType, TDictType>> expression, TKey key, TElement value)
             where TDictType : IDictionary<TKey, TElement>
@@ -53,6 +98,19 @@ namespace NGineer
             });
         }
 
+        /// <summary>
+        /// Add a given value into the selected list type
+        /// Other entries may already be present in the list
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TList>"/>
+        /// </param>
+        /// <param name="value">
+        /// A <see cref="TElement"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TList>"/>
+        /// </returns>
         public static ITypedBuilder<TList> AddEntry<TList, TElement>(this ITypedBuilder<TList> builder,
             TElement value)
             where TList : IList<TElement>
@@ -64,7 +122,22 @@ namespace NGineer
             });
         }
 
-
+        /// <summary>
+        /// Add a given value into the selected member's list
+        /// Other entries may already be present in the list
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TType>"/>
+        /// </param>
+        /// <param name="expression">
+        /// A <see cref="Expression<Func<TType, TList>>"/>
+        /// </param>
+        /// <param name="value">
+        /// A <see cref="TElement"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TType>"/>
+        /// </returns>
         public static ITypedBuilder<TType> AddEntry<TType, TList, TElement>(this ITypedBuilder<TType> builder,
             Expression<Func<TType, TList>> expression,
             TElement value)
@@ -78,41 +151,63 @@ namespace NGineer
             });
         }
 
-
+        /// <summary>
+        /// Set the member with a sequentially increasing number starting from 0
+        /// This number is not reset when a new build session is started
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, System.Int32>>"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> SetConsecutiveInt<TObj>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, int>> property)
         {
             return builder.SetConsecutiveInt(property, 0);
         }
 
-
+        /// <summary>
+        /// Set the member with a sequentially increasing number starting from the specified starting point
+        /// This number is not reset when a new build session is started
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, System.Int32>>"/>
+        /// </param>
+        /// <param name="startIndex">
+        /// Starting number to commence number from.  The first created object receives this value
+        /// A <see cref="System.Int32"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> SetConsecutiveInt<TObj>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, int>> property, int startIndex)
         {
             return builder.Set(property, (obj, buildr, session) => startIndex++);
         }
 
-
-        public static ITypedBuilder<TObj> SetConsecutiveInt<TObj>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, string>> property)
-        {
-            return builder.SetConsecutiveInt(property, 0);
-        }
-
-
-        public static ITypedBuilder<TObj> SetConsecutiveInt<TObj>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, string>> property, int startIndex)
-        {
-            return builder.Set(property, (obj, buildr, session) => startIndex++.ToString());
-        }
-
-
+        /// <summary>
+        /// Numbers sibling items with the same immediate parent with a sequentially increasing number starting from 0
+        /// This number is reset to 0 every new parent object, and remembers the last number used for a given parent hierarchy
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, System.Int32>>"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> SetSiblingConsecutiveInt<TObj>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, int>> property)
         {
             var sequence = Sequences.SiblingConsecutiveInt();
             return builder.Set<int>(property, (o, b, s) => sequence.Next(o, b, s));
-        }
-
-
-        public static TObj Build<TObj>(this ITypedBuilder<TObj> builder)
-        {
-            return builder.Build<TObj>();
         }
 
 
@@ -121,32 +216,62 @@ namespace NGineer
             return builder.SetCollectionSize<TObj>(min, max).For<TObj>();
         }
 
-
+        /// <summary>
+        /// Populate a member using the sessioned builder
+        /// This is intended to allow easy restoring after ignoring all members on a given type
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, TReturnType>>"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> Set<TObj, TReturnType>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, TReturnType>> property)
         {
-            return builder.Set(property, (o, b, s) => b.Build<TReturnType>()).For<TObj>();
+            return builder.Set(property, (o, b, s) => b.Build<TReturnType>());
         }
 
-
+        /// <summary>
+        /// Simple factory overload that populates a member with the given result of the parameterless getter
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, TReturnType>>"/>
+        /// </param>
+        /// <param name="getter">
+        /// A <see cref="Func<TReturnType>"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> Set<TObj, TReturnType>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, TReturnType>> property, Func<TReturnType> getter)
         {
             return builder.Set(property, (o, b, s) => getter());
         }
 
-
+        /// <summary>
+        /// Set the value of a member from a specified list of options (repeats allowed)
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, TReturnType>>"/>
+        /// </param>
+        /// <param name="entries">
+        /// A <see cref="IEnumerable<TReturnType>"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> SetFromCollection<TObj, TReturnType>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, TReturnType>> property, IEnumerable<TReturnType> entries)
         {
             return builder.Set(property, (o, b, s) => s.Random.OneFromList(entries));
-        }
-
-
-        public static ITypedBuilder<TObj> SetString<TObj>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, string>> property, int maxLength)
-        {
-            return builder.Set(property, (o, b, s) =>
-            {
-                var str = b.Build<string>();
-                return str.Substring(0, Math.Min(str.Length, maxLength));
-            });
         }
 
         /// <summary>
@@ -168,6 +293,22 @@ namespace NGineer
         }
 
 
+        /// <summary>
+        /// Set the member with a constructed instance of one of the specified types
+        /// A random type is selected each time a member is constructed
+        /// </summary>
+        /// <param name="builder">
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </param>
+        /// <param name="property">
+        /// A <see cref="Expression<Func<TObj, TReturnType>>"/>
+        /// </param>
+        /// <param name="types">
+        /// A <see cref="Type[]"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="ITypedBuilder<TObj>"/>
+        /// </returns>
         public static ITypedBuilder<TObj> SetAsType<TObj, TReturnType>(this ITypedBuilder<TObj> builder, Expression<Func<TObj, TReturnType>> property, params Type[] types)
         {
             return builder.Set(property, (o, b, s) => (TReturnType)b.Build(s.Random.OneFromList((IEnumerable<Type>)types)));
